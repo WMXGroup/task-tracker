e75c5f403c3756cbea927c97b5346d227b8a9be3
Adam DePietropaolo <WMXGroup@gmail.com>
2020-08-16 10:56:40 -0400

    Updates

---
+++ precache-manifest.1eb986f9815e3aa13c4bacf974da7420.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "410e82d5ca187b09be37f108cb760bf3",
+    "url": "/task-tracker/index.html"
+  },
+  {
+    "revision": "d205b28541aa6d6cfb08",
+    "url": "/task-tracker/static/js/2.af838c7f.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/task-tracker/static/js/2.af838c7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90ca510acfd20f49ca42",
+    "url": "/task-tracker/static/js/main.712279c9.chunk.js"
+  },
+  {
+    "revision": "44e2746e3f7572562cbe",
+    "url": "/task-tracker/static/js/runtime-main.290efcd8.js"
+  }
+]);
