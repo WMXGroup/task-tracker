34c014bb114f1e8ba4254ffd12f6bb65ec5e752d
Adam DePietropaolo <WMXGroup@gmail.com>
2022-01-15 20:04:59 -0500

    Updates

---
+++ precache-manifest.763983c712be3fa55d5246b830c2b5b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8e216a040d80a81ca50226074effff0",
+    "url": "/task-tracker/index.html"
+  },
+  {
+    "revision": "1230b45a308bc5e1deb9",
+    "url": "/task-tracker/static/js/2.5a352593.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/task-tracker/static/js/2.5a352593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33062c6d76511e40660a",
+    "url": "/task-tracker/static/js/main.349f8cab.chunk.js"
+  },
+  {
+    "revision": "44e2746e3f7572562cbe",
+    "url": "/task-tracker/static/js/runtime-main.290efcd8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/task-tracker/precache-manifest.51c121a413f91deb4e6ec830813926ca.js"
+  "/task-tracker/precache-manifest.763983c712be3fa55d5246b830c2b5b2.js"
 );
 
 self.addEventListener('message', (event) => {
