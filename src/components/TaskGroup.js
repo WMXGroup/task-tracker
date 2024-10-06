1565e81e980cd9c3303793409e3788ae917dd0b0
Adam D <adamd93@gmail.com>
2024-10-06 14:27:15 -0400

    updated

--- src/components/TaskGroup.js
+++ src/components/TaskGroup.js
@@ -28,10 +28,13 @@ export default class TaskGroup extends Component {
           if (tasks[i].dates[j].date === header && tasks[i].dates[j].date >= moment(startDate).format('YYYY-MM-DD') && tasks[i].type !== 'Tracking'){
             groupTasks.push(tasks[i])
           }
-          if (tasks[i].dates[j].date === header && (tasks[i].dates[j].state === 'open' && (tasks[i].type === 'Deadline' || tasks[i].type === 'Periodic'))){
+          if (tasks[i].dates[j].date === header && tasks[i].dates[j].date < moment(startDate).format('YYYY-MM-DD') &&(tasks[i].dates[j].state === 'open' && (tasks[i].type === 'Deadline' || tasks[i].type === 'Periodic'))){
             groupTasks.push(tasks[i])
           }
           if (tasks[i].dates[j].date === header && tasks[i].dates[j].date >= moment(startDate).format('YYYY-MM-DD') && tasks[i].type === 'Tracking' & parseInt(tasks[i].recurDays,10) > 1){
+            groupTasks.push(tasks[i])
+          }
+          if (tasks[i].dates[j].date === header && tasks[i].dates[j].date >= moment(startDate).format('YYYY-MM-DD') && tasks[i].type === 'Tracking' & parseInt(tasks[i].recurDays,10) === 1){
             groupTasks.push(tasks[i])
           }
         }  
